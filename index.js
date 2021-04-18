860d1db7f0fd4e000321f868c18cd091a16481a7
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-18 15:49:05 -0500

    Added prompts.js to separate functionality.

--- index.js
+++ index.js
@@ -1,86 +1,26 @@
+// Require modules
 const mysql = require("mysql");
-const inquirer = require("inquirer");
+const menu = require("./prompts")
 
+// Create connection variable for mysql connection info
 const connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Listen on port 3306
   port: 3306,
 
-  // Your username
+  // Set username
   user: "root",
 
-  // Your password
+  // Set password and database
   password: "",
   database: "employeetracker_DB",
 });
 
+// Connect to mysql
 connection.connect((err) => {
   if (err) throw err;
   console.log("Welcome to the Employee Tracker Database!");
-  primaryMenu();
+  menu.primaryMenu(connection);
 });
 
-function primaryMenu(){
-    inquirer.prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            'List',
-            'Add',
-            'Update',
-            'Delete',
-            'Exit',
-        ],
-    })
-    .then((response) => secondaryMenu(response));
-}
-
-function secondaryMenu(response){
-    switch (response.action) {
-        case "List":
-          listMenu();
-          break;
-
-        case "Add":
-          addMenu();
-          break;
-
-        case "Update":
-          updateMenu();
-          break;
-
-        case "Delete":
-          deleteMenu();
-          break;
-
-        case "Exit":
-          connection.end();
-          process.exit();
-
-        default:
-          console.log(`Invalid action: ${answer.action}`);
-          break;
-      }
-}
-
-function listMenu(){
-    console.log("List");
-    primaryMenu();
-}
-
-function addMenu(){
-    console.log("Add");
-    primaryMenu();
-}
-
-function updateMenu(){
-    console.log("Update");
-    primaryMenu();
-}
-
-function deleteMenu(){
-    console.log("Delete");
-    primaryMenu();
-}
